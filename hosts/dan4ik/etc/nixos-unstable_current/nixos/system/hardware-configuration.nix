d888f97cab02240ed1be18e0a37884fde1be70cc
dan4ik <6057430gu@gmail.com>
2021-08-12 16:49:48 +0700

    dan4ik: bump hardware-configuration.nix

--- hosts/dan4ik/etc/nixos-unstable_current/nixos/system/hardware-configuration.nix
+++ hosts/dan4ik/etc/nixos-unstable_current/nixos/system/hardware-configuration.nix
@@ -8,26 +8,27 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/560E-4C4B";
+      fsType = "vfat";
+    };
 
   fileSystems."/" =
     {
-      device = "/dev/sda5";
+      device = "/dev/disk/by-uuid/6d2b6c02-fcfb-4572-bd47-00a704fd284c";
       fsType = "xfs";
     };
 
   fileSystems."/home" =
     {
-      device = "/dev/sda6";
+      device = "/dev/disk/by-uuid/6db079d5-ca79-4936-a895-d57b59178f97";
       fsType = "xfs";
     };
 
-  fileSystems."/boot" =
-    {
-      device = "/dev/sda2";
-      fsType = "vfat";
-    };
-
   swapDevices = [ ];
+
 }
