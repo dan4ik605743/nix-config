ec0cfead0ec33c714263654faf2a094a80fdbba7
dan4ik <6057430gu@gmail.com>
2021-10-07 23:30:10 +0700

    overlays/derivation.nix: updated

--- hosts/dan4ik/etc/nixos/system/overlays/derivations.nix
+++ hosts/dan4ik/etc/nixos/system/overlays/derivations.nix
@@ -2,13 +2,4 @@ final: prev: {
   winetricks = prev.winetricks.override {
     wine = prev.wineWowPackages.staging;
   };
-  ripgrep = prev.ripgrep.override {
-    withPCRE2 = true;
-  };
-  viber = prev.viber.overrideAttrs (attr: {
-    src = prev.fetchurl {
-      url = "https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb";
-      sha256 = "sha256-EDekjXTK7zPRI7Fm2iv7H+j6Z1kLhmns8lsxT0E3Qmc=";
-    };
-  });
 }
