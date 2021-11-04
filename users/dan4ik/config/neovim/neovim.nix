df37a9019681f92ba3f25dfc1cd351d8c90c4414
dan4ik <6057430gu@gmail.com>
2021-11-05 01:35:26 +0700

    config/neovim: updated

--- users/dan4ik/config/neovim/neovim.nix
+++ users/dan4ik/config/neovim/neovim.nix
@@ -9,7 +9,6 @@
     Plug 'LnL7/vim-nix'
     Plug 'OmniSharp/omnisharp-vim'
     Plug 'vim-airline/vim-airline'
-    Plug 'vim-airline/vim-airline-themes'
     Plug 'puremourning/vimspector'
     Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
     Plug 'junegunn/fzf.vim'
@@ -18,21 +17,29 @@
     Plug 'preservim/tagbar'
     Plug 'mg979/vim-visual-multi', {'branch': 'master'}
     Plug 'lukas-reineke/indent-blankline.nvim'
-    Plug 'morhetz/gruvbox'
+    Plug 'arcticicestudio/nord-vim'
   call plug#end()
+
+  " Theme
+  colorscheme nord
+  set background=dark
+  let g:nord_uniform_diff_background = 1
+  let g:nord_cursor_line_number_background = 1
+  let g:nord_uniform_status_lines = 1
+  let g:nord_italic = 1
+  let g:nord_italic_comments = 1
+  let g:nord_underline = 1
 
   " Settings
   set number
   set mouse=a
   set clipboard+=unnamedplus
-  set background=dark
   set encoding=utf-8
   set nobackup       
   set nowritebackup  
   set noswapfile
   set autochdir
   scriptencoding utf-8
-  colorscheme gruvbox
   syntax on
   :set tabstop=4
   :set shiftwidth=4
@@ -56,7 +63,7 @@
 
   " AirLine
   let g:airline_powerline_fonts=1
-  let g:airline_theme='minimalist'
+  let g:airline_theme='nord'
 
   " OmniSharp
   let g:OmniSharp_server_path = '${pkgs.omnisharp-roslyn}/bin/omnisharp'
