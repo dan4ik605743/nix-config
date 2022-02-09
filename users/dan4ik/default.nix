ab05a2225336a43c2620063d8ad2e25bdbe6ca82
dan4ik <6057430gu@gmail.com>
2022-02-09 15:58:41 +0700

    dan4ik/default.nix: fix i3.keybindings

--- users/dan4ik/default.nix
+++ users/dan4ik/default.nix
@@ -139,8 +139,8 @@ in
           "XF86AudioPlay" = "exec playerctl play-pause";
           "XF86AudioPrev" = "exec playerctl previous";
           "XF86AudioNext" = "exec playerctl next";
-          "XF86AudioRaiseVolume" = "exec amixer -q set PCM 4%-";
-          "XF86AudioLowerVolume" = "exec amixer -q set PCM 4%+";
+          "XF86AudioRaiseVolume" = "exec amixer -q set PCM 4%+";
+          "XF86AudioLowerVolume" = "exec amixer -q set PCM 4%-";
           "XF86AudioMute" = "exec amixer -q set PCM toggle";
           "XF86TouchpadToggle" = "exec synclient TouchpadOff=$(synclient -l | grep -c 'TouchpadOff.*=.*0')";
           "Print" = "exec maim -su | xclip -selection clipboard -t image/png";
