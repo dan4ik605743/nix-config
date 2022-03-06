5f182ee485fdf935db863aff41d760c28a21352d
dan4ik <6057430gu@gmail.com>
2022-03-06 09:45:05 +0700

    neovim/coc-settings.nix: add support ccls

--- users/dan4ik/config/neovim/coc-settings.nix
+++ users/dan4ik/config/neovim/coc-settings.nix
@@ -4,6 +4,16 @@
       "nix": {
         "command": "rnix-lsp",
         "filetypes": ["nix"]
+      },
+      "ccls": {
+        "command": "ccls",
+        "filetypes": ["c", "cpp", "c++" ],
+        "rootPatterns": [".ccls", "compile_commands.json", ".git/", ".hg/"],
+        "initializationOptions": {
+          "cache": {
+            "directory": "/tmp/ccls"
+          }
+        }
       }
     }
   }
