ca55586412a3c55c527ab3282aa2dadf9fe136be
dan4ik <6057430gu@gmail.com>
2022-03-06 08:11:18 +0700

    config/nix.nix: fix out of update nix

--- config/nix.nix
+++ config/nix.nix
@@ -1,10 +1,8 @@
 { inputs, system, nixpkgs }:
 
 rec {
-  package = nixpkgs.legacyPackages."${system}".nixFlakes;
-  trustedBinaryCaches = binaryCaches;
-  maxJobs = 1;
-
+  package = nixpkgs.legacyPackages."${system}".nix;
+ 
   extraOptions = ''
     experimental-features = nix-command flakes
   '';
@@ -20,39 +18,33 @@ rec {
       "repl=${path}/repl.nix"
     ];
 
-  trustedUsers = [
-    "root"
-    "dan4ik"
-  ];
-
-  binaryCaches = [
-    "https://cache.nixos.org?priority=10"
-    "https://cache.ngi0.nixos.org/"
-    "https://nix-community.cachix.org"
-    "https://dan4ik605743.cachix.org"
-  ];
-
-  binaryCachePublicKeys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "dan4ik605743.cachix.org-1:rhJb/S+2G33sj0wR2fXp0WqMKPCYHpeWG2AjS4dwUaA="
-  ];
-
   registry = {
     system.flake = inputs.self;
     default.flake = inputs.nixpkgs;
     home-manager.flake = inputs.home;
   };
 
-  optimise = {
-    automatic = true;
-    dates = [ "13:00" ];
-  };
+  settings = rec {
+    max-jobs = 1;
+    trusted-substituters = substituters;
 
-  gc = {
-    automatic = false;
-    dates = "weekly";
-    options = "-d";
+    trusted-users = [
+      "root"
+      "dan4ik"
+    ];
+
+    substituters = [
+      "https://cache.nixos.org?priority=10"
+      "https://cache.ngi0.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://dan4ik605743-nur.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "dan4ik605743-nur.cachix.org-1:emiEpwGzec0g1z5juvp8i1aqe1W5hICsLauv9UZ6OJw="
+    ];
   };
 }
